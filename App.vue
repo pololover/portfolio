bc6c9079270a093675c0601952d6a711cbbcc551
cheon seung hyun <70146658+pololover@users.noreply.github.com>
2022-01-27 19:58:45 +0900

    footer부분,  헬맵 설명

--- App.vue
+++ App.vue
@@ -63,12 +63,131 @@
       </div>
     </div>
     <!-- Projects -->
-    <div>
+    <div class="Project_wrap">
+      <div class="project_title">
+        Project
+      </div>
+      <div class="fran_whiteBox"> 
+      <!-- franchise 프로젝트 -->
+      <div class="project_fran_title">
+          신메뉴드루와
+      </div>
+      <div>
+        <!-- 프랜차이즈 설명부분 -->
+        <div class="fran_expl"> 
+            <!--franchise 정보  -->
+          <div class="fran_primary">프랜차이즈 신메뉴에 대해서 정보를 공유하고 토론할 수 있는 웹사이트입니다.<span style="font-size:0.8em;color:gray;"> (1人)</span></div>
+            <!-- 기술 스택 -->
+          <ul>
+            <li>Frontend: 100%</li>
+            <li>Backend: 100%</li>
+            <li>Tech stack: vue, firebase, aws</li>
+            <li>Library: tinymice</li>
+          </ul>
+          <!--배운 것들-->
+          <div class="fran_learn">
+            aws, firebase 등을 통하여 데이터 토큰 저장방식에 대하여 고민할 수 있었고, Vue에서 데이터를 CRUD 하는데 있어 배움이 있었던 프로젝트입니다.
+          </div>
+          <div>
+            Vuex를 이용해 상태관리에 대한 이해도를 높였습니다. 
+          </div>
+          <!-- 깃허브 -->
+          <div class="fran_git">
+            깃허브: <a href="https://github.com/pololover/FranchiseNew">https://github.com/pololover/FranchiseNew</a>
+          </div>
+        </div>
+        <div class="container">
+         <div class="wrapper">
+          <input type="checkbox" @click="onFocus" class="watch">
+            <div class="video">
+          <iframe border="0" width="660" height="415" src="https://www.youtube.com/embed/F3t98EjBnmo?autoplay=1&mute=1&controls=1&loop=1" title="YouTube video player" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <div class="text">
+            <span data-text="Watch the video"></span>
+          </div>
+          </div>
+        </div>
+       </div>
+      </div>
+      <div class="Project_padd3">
+        <div class="project_fran_title">맛집일기</div>
+        <div class="project_health_wrap">
+          <div class="health_description">
+            <div class="fran_primary">유명 인스타그래머에게 맛집을 추천받을 수 있는 웹사이트입니다.<span style="font-size:0.8em;color:gray;"> (1人)</span></div>
+            <ul>
+              <li>Frontend: 100%</li>
+              <li>Tech stack: vue</li>
+              <li>Library: Swiper</li>
+            </ul>
+          <!-- 맛집일기 learn -->
+          <div class="diary_learn">
+            VueJs를 이용하여 UI디자인과 애니매이션에 대한 이해도를 높이기 위해 이에 포커스를 두어 프로젝트를 진행했습니다. 
+          </div>
+          <div>
+            사용자가 정보를 가시적으로 볼 수 있도록 Swiper 라이브러리를 사용했고 이를 처음 적용함으로써 Vue의 Composition API에서 어떻게 정의하고 호출하는지 알게 되었습니다.
+          </div>
+          <!-- git_맛집일기 -->
+          <div class="diary_git">
+            깃허브:<a href="https://github.com/pololover/Mukhondon">https://github.com/pololover/Mukhondon</a>
+          </div>
+        </div>
+          <div class="text_photo">
+            <img src="../src/images/diary.jpg" class="diary_img">
+            <div class="text_in_img">Vue 컴포넌트화 및 Swiper에 대한 이해증가</div>
+          </div>
+        </div>
+      </div>
+      <div class="Project_padd3">
+        <div class="project_fran_title">
+          헬맵
+        </div>
+        <!-- 헬맵 내용 전체 -->
+        <div class="project_health_wrap">
+          <div class="health_description">
+            
+            <div class="fran_primary">COVID-19로 인한 실내헬스장에 가기 두려워지는 사람들을 위해 개발한 웹사이트입니다.<span style="font-size:0.8em;color:gray;"> (3人)</span></div>
+          <div>
+            <ul style="margin-bottom:2em;">
+            <li>Frontend: 70%</li>
+            <li>Backend: 50%</li>
+            <li>Tech stack: Jsp, JDBC, SQL</li>
+          </ul>
+          <!-- 헬맵 내용 설명 -->
+          <div>
+            처음 진행한 협업 프로젝트로 jsp와 html, js, css로 전반적인 프론트엔드 부분을 구현하였고, sql과 JDBC를 통해 데이터베이스 부분을 처리했습니다. 
+          </div>
+          <div class="diary_learn">
+            Ajax호출을 통해 비동기 함수 및 데이터에 대해 배울 수 있었고 카카오 Map API를 사용해 마커 및 Map객체 사용방법에 대해 학습했습니다.
+          </div>
+          <div class="diary_git">
+            깃허브:<a href="https://github.com/pololover/healthymap">https://github.com/pololover/healthymap</a>
+          </div>
+            </div>
+          </div>
+          <!-- img gsap -->
+          <div>
+            <!-- img gsap 애니메이션 사진-1 -->
+            <div>
+              <img src="../src/images/health-1.jpg" class="gsap_img">
+            </div>
+            <!-- img gsap 애니메이션 사진-2 -->
+            <div>
 
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <!-- Career -->
-    <div>
-
+    <!-- Contact -->
+    <div class="contact_wrap">
+      <div class="flex_contact">
+        <img src="../src/images/github.png" class="contact_imgs">
+        <img src="../src/images/tistory.png" class="contact_imgs_tistory">
+      </div>
+      <!-- text -->
+      <div class="right_text">
+        © 2021. Cheon Seung Hyun. All rights reserved.
+      </div>
     </div>
   </div>
 </template>
@@ -136,6 +255,9 @@ export default {
       this.$refs.logo_imgs10.style.visibility = 'visible';
       this.$refs.logo_imgs10.style.animation = 'bottom 1.3s linear';
       }
+    },
+    onFocus : function() {
+      
     }
   },
   created(){
@@ -156,19 +278,21 @@ export default {
      this.userInfo = this.map(u=> u.shift(),this.map(u=>Object.keys(u),this.user));
      console.log(this.user);
      this.userInfoValue = this.map(u=>u.shift(),this.map(u=>Object.values(u),this.user));
-     window.scrollTo(0, 0);
      document.addEventListener('scroll', this.scrollEvent)
    }
 }
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Nanum+Myeongjo:wght@700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700;800&family=Noto+Serif+KR:wght@700;900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Serif+KR:wght@700;900&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&family=Noto+Serif+KR:wght@700;900&display=swap');
 body {
   margin:0px;
-  height: 10000px;
+  height: 3600px;
   overflow-x: hidden;
-  font-family: 'Nanum Myeongjo', serif;
+  font-weight:400;
+  font-family: 'Noto Serif KR', serif;
   }
 
 .introd_wrap, .About_wrap{
@@ -181,11 +305,16 @@ body {
 }
 
 .header_content{
+  z-index: 3;
   position:fixed;
+  background-color:beige;
+
+  color:black;
+  height:40px;
   top:0px;
   left:0px;
   right:0px;
-  margin: 1em 4em;
+  padding: 1em;
   display:flex;
   align-items: center;
   justify-content: space-between;
@@ -213,7 +342,7 @@ body {
 }
 
 
-.About_title, .Skill_title{
+.About_title, .Skill_title, .project_title{
   font-size:2em;
   border-bottom: 1px solid black;
   margin-right:50px;
@@ -249,7 +378,7 @@ body {
 
 .Skills_wrap{
   background-color:whitesmoke;
-  padding: 2em 6em 5em;
+  padding: 2em 6em 4em;
 }
 
 .skill_img{
@@ -351,5 +480,173 @@ body {
   width : 8em;
 }
 
+.Project_wrap{
+  background-color:darkgray;
+  padding: 2em 6em 6em;
+  font-family: 'Noto Sans KR', sans-serif;
+  font-weight:400;
+}
 
+.Project_padd {
+  margin-top:1em;
+  margin-bottom:1em;
+  padding: 3em;
+  border-radius: 30px;
+  background-color:rgba(255, 255, 255, 0.589);
+  height:20em;
+}
+
+.Project_padd3{
+  background-color:rgba(255, 255, 255, 0.589);
+  height:30em;
+  padding-bottom:4em;
+  position:relative;
+  padding-left:2em;
+  padding-right:1em;
+}
+
+.project_fran_title {
+  display:flex;
+  justify-content: center;
+  margin-right:4em;
+  font-size:1.7em;
+  margin-top:1em;
+  font-weight:600;
+}
+
+.fran_whiteBox{
+  background-color:rgb(255, 255, 255, 0.5);
+  padding : 0 1em;
+}
+
+.fran_expl {
+  position:absolute;
+  margin-top:3em;
+  margin-left:1em;
+  width :350px;
+}
+.fran_expl_title{
+  display:flex;
+  font-size:1.5em;
+  justify-content: center;
+}
+.fran_primary{
+  line-height:1.4em;
+  margin-bottom:3em;
+  font-weight:800;
+  font-family: 'Nanum Gothic', sans-serif;
+}
+.fran_learn {
+  margin-top : 2em;
+  margin-bottom :1em;
+}
+.fran_git {
+  margin-top: 2em;
+  font-size: 0.9em;
+}
+
+.project_health_wrap{
+  display:flex;
+  flex :1;
+  margin-top:2em;
+}
+.health_description{
+  width: 360px;
+  margin-right:1.5em;
+}
+
+.text_in_img{
+  position:absolute;
+  opacity: 0;
+  top:35%;
+  left:28%;
+  font-size:1.2em;
+  font-weight:700;
+}
+
+.text_photo{
+  position:relative;
+  display:flex;
+  flex :1;
+  margin-left:6em;
+}
+
+.diary_img{
+  width:100%;
+  height:370px;
+  border-radius: 5px;
+}
+
+
+.text_photo:hover .diary_img{
+  opacity: 0.5;
+  filter:blur(5px);
+}
+
+.text_photo:hover .text_in_img{
+  opacity: 1;
+}
+
+.project_diary_title{
+  margin-left: 1em;
+  font-weight:800;
+  font-family: 'Nanum Gothic', sans-serif;
+}
+
+.diary_learn{
+  margin : 2em 0;
+}
+
+.diary_git{
+  margin-top:1em;
+  font-size:0.9em;
+}
+
+.gsap_img{
+  width : 250px;
+  height:400px;
+}
+
+.contact_wrap{
+  padding: 2em;
+}
+
+.flex_contact{
+  display:flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.contact_imgs{
+  width:50px;
+  height:25px;
+  cursor: pointer;
+}
+
+.contact_imgs:hover{
+  opacity: 0.5;
+}
+
+.contact_imgs_tistory{
+  width:30px;
+  height:25px;
+  cursor: pointer;
+}
+
+.contact_imgs_tistory:hover {
+  opacity: 0.5;
+}
+
+.right_text{
+  margin-top:1em;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  font-size:0.8em;
+}
+
+</style>
+
+<style lang="scss">
+@import "@/assets/style/sstyle.scss"
 </style>
